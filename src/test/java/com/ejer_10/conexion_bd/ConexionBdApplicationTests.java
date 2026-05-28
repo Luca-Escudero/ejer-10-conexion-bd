e0a68e17d44ba97c6ddf6f7d87d2f0c5f0eb1efa
luca <lucaescudero06@gmail.com>
2026-05-28 20:17:51 -0300

    first commit

---
+++ src/test/java/com/ejer_10/conexion_bd/ConexionBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejer_10.conexion_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
